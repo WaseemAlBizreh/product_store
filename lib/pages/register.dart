8bb2b939d4c89be1fa91872cdf0d9061d91e3ab5
AmaleaBannour <amaleabannour062@gmail.com>
2021-12-22 10:55:37 +0200

    without button

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -7,6 +7,9 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+
+  bool passvisible = true;
+
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
@@ -15,40 +18,94 @@ class _RegisterState extends State<Register> {
           height: double.infinity,
           decoration: const BoxDecoration(
             image: DecorationImage(
-              image: AssetImage("assets/image/bg.jpg"),
+              image:AssetImage("assets/image/bg.jpg"),
               fit: BoxFit.cover,
             ),
           ),
-          child: Column(
-            children: [
-              SizedBox(height: constraints.maxHeight *0.1,),
-              Container(
-                width: double.infinity,
-                height: constraints.maxHeight * 0.3,
-                child: const CircleAvatar(
-                  backgroundImage: AssetImage("assets/image/logo.png"),
-                  backgroundColor: Colors.transparent,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: constraints.maxHeight *0.1,),
+                Container(
+                  width: double.infinity,
+                  height: constraints.maxHeight * 0.3,
+                  child: const CircleAvatar(
+                    backgroundImage: AssetImage("assets/image/logo.png"),
+                    backgroundColor: Colors.transparent,
+                  ),
                 ),
-              ),
-              Container(
-                  height: constraints.maxHeight * 0.6,
-                  child : Card (
-                    margin: EdgeInsets.all(constraints.minWidth *0.04),
-                    child: Column(
-                      children: [
-                        Container(
-                          padding: EdgeInsets.all(constraints.minWidth * 0.1),
-                          child: TextFormField(
-                            autocorrect: true,
-                            cursorColor: Colors.pinkAccent,
-                            keyboardType: TextInputType.text,
+                Container(
+                    height: constraints.maxHeight * 0.55,
+                    child : Card (
+                      margin: EdgeInsets.all(constraints.minWidth *0.05),
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.fromLTRB(constraints.minWidth *0.07,
+                                constraints.minHeight *0.03,
+                                constraints.minWidth *0.07,
+                                constraints.minHeight *0.02),
+                            child: TextFormField(
+                              autocorrect: true,
+                              cursorColor: Colors.pinkAccent,
+                              keyboardType: TextInputType.text,
+                              textInputAction: TextInputAction.next,
+                              decoration: InputDecoration(
+                                hintText: "User Name",
+                                hintStyle: TextStyle(fontSize: 15),
+                                icon: Icon(Icons.person)
+                              ),
+                            ),
                           ),
-                        )
-                      ],
-                    ),
-                  )
-              )
-            ],
+                          Container(
+                            padding: EdgeInsets.fromLTRB(constraints.minWidth *0.07,
+                                constraints.minHeight *0.02,
+                                constraints.minWidth *0.07,
+                                constraints.minHeight *0.02),
+                            child: TextFormField(
+                              autocorrect: true,
+                              cursorColor: Colors.pinkAccent,
+                              keyboardType: TextInputType.emailAddress,
+                              decoration: InputDecoration(
+                                  hintText: "Email",
+                                  hintStyle: TextStyle(fontSize: 15),
+                                  icon: Icon(Icons.email)
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.fromLTRB(constraints.minWidth *0.07,
+                                constraints.minHeight *0.02,
+                                constraints.minWidth *0.07,
+                                constraints.minHeight *0.02),
+                            child: TextFormField(
+                              autocorrect: true,
+                              cursorColor: Colors.pinkAccent,
+                              keyboardType: TextInputType.visiblePassword,
+                                obscureText : passvisible,
+                              decoration: InputDecoration(
+                                  hintText: "Password",
+                                  hintStyle: TextStyle(fontSize: 15),
+                                  icon: Icon(Icons.lock),
+                                suffixIcon: IconButton(
+                                  icon: Icon(
+                                    passvisible ? Icons.visibility : Icons.visibility_off
+                                  ),
+                                  onPressed: () {
+                                    setState(() {
+                                      passvisible = !passvisible;
+                                    });
+                                  },
+                                )
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                )
+              ],
+            ),
           ),
         ),
       ),
