a10f315fb8779f6a6af2805b71526713b6914368
WaseemAlBizreh <waseem.bez@gmail.com>
2022-01-01 20:25:06 +0200

    api_product

--- lib/model/products_model.dart
+++ lib/model/products_model.dart
@@ -10,15 +10,30 @@ class Products_Response_Model{
   late  String image_path;
 
   Products_Response_Model({
-      required this.name,
-      required this.id,
-      required this.image_path});
+    required this.name,
+    required this.id,
+    required this.image_path
+  });
 
   factory Products_Response_Model.fromJson(Map<String, dynamic> jsondata) {
     return Products_Response_Model(
       name : jsondata['name'] ?? "" ,
       id : jsondata['id'] ?? 0,
       image_path: jsondata['image_path'] ?? "",
+    );
+  }
+}
+
+class product_list_view {
+  late List<dynamic> products;
+
+  product_list_view({
+    required this.products
+  });
+
+  factory product_list_view.fromJson(jsonData) {
+    return product_list_view(
+      products: jsonData,
     );
   }
 }
