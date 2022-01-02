10f2d7e74c68e4de07b8ef0aa54ee129f6882b70
AmaleaBannour <amaleabannour062@gmail.com>
2022-01-02 16:01:31 +0200

    profile2

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,17 +1,52 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-
-class profile extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('My Account'),
-      ),
-      body: Container(
-
-      ),
-    );
-  }
-}
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:product_store/ApI/auth_api.dart';
+// import 'package:product_store/ApI/product_listview.dart';
+// import 'package:provider/provider.dart';
+//
+// class profile extends StatelessWidget {
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return ChangeNotifierProvider(
+//       create: (_) => pro_auth(),
+//       child: Consumer<pro_auth>(
+//       builder: (context, value, child) {
+//       child: LayoutBuilder(builder: (context, constraints) {
+//         return Scaffold(
+//           appBar: AppBar(
+//             title: Text('My Account'),
+//           ),
+//           body: Container(
+//             child: Card(
+//               child: Container(
+//                 child: Row(
+//                   children: [
+//                     Container(
+//                       width: constraints.maxWidth * 0.4,
+//                       child: Image.network(
+//                           'https://www.gpdecor.nl/wp-content/uploads/2014/02/Product-300x232.jpg',
+//                           //change value.productslist[index].image_path
+//                           fit: BoxFit.fitWidth),
+//                     ),
+//                     SizedBox(
+//                       width: constraints.maxWidth * 0.1,
+//                     ),
+//                     Container(
+//                       child: ListTile(
+//                         title: ,
+//                         subtitle: ,
+//                       ),
+//                     )
+//                   ],
+//                 ),
+//               ),
+//             ),
+//           ),
+//         );
+//
+//       }
+//       ),
+//     );
+//   }
+// }
